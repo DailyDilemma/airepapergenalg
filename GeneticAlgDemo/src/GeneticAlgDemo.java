b271da57fac2e205214377f6ce0ef7a3ccc8f701
Josh Westlake <jwestlake@zerocomplexity.ca>
2015-12-02 23:01:09 -0600

    Added more steps to the algorithm

--- GeneticAlgDemo/src/GeneticAlgDemo.java
+++ GeneticAlgDemo/src/GeneticAlgDemo.java
@@ -1,38 +1,40 @@
 /**
  * 
  */
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  * @author Josh Westlake
  *
  */
 public class GeneticAlgDemo {
 	
-	static final int POPULATION = 40;
-	static final int MIN_PER_INTERVAL = 15;
-	static final int MIN_PER_DAY = 3600;
-	static final int INTERVALS_PER_DAY = MIN_PER_INTERVAL / MIN_PER_DAY;
-	
-
+	static final int POPULATION = 10;
+	static final int INTERVALS_PER_DAY = 96; // Each interval is 15 minutes.
+	static int evalResults[] = new int[POPULATION]; // Stores the fitness measure for each population member
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		boolean population[][] = new boolean[INTERVALS_PER_DAY][POPULATION];
-		System.out.println("Hello World!");			
+		boolean population[][] = new boolean[POPULATION][INTERVALS_PER_DAY];
+		//System.out.println("Hello World!");			
 		
 		// Create a population of random schedules
-		initialize(population);
-		
+		initialize(population);				
+		//printPopulation(population, false);
+		evaluate(population);
+		printPopulation(population, true);
+		population = selection(population);
 	}
 	
 	public static void initialize(boolean[][] population) {
 		
 		// Initialize an array of randomized possible schedules
-		for (int i=0; i<INTERVALS_PER_DAY; i++) {
-			for (int j=0; j<POPULATION; j++) {
+		for (int i=0; i<POPULATION; i++) {
+			for (int j=0; j<INTERVALS_PER_DAY; j++) {
 				if (Math.random() < 0.5) {
 					population[i][j] = true;
 				} else {
@@ -42,5 +44,120 @@ public class GeneticAlgDemo {
 		}
 		
 	}
+	
+	public static void printPopulation(boolean[][] population, boolean withScores) {
+		
+		// Initialize an array of randomized possible schedules
+		for (int i=0; i<POPULATION; i++) {
+			System.out.print(String.format("%2d: ", i+1));
+			for (int j=0; j<INTERVALS_PER_DAY; j++) {
+				if (population[i][j]) {
+					System.out.print("X");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			
+			if (withScores) {
+				System.out.println(" (fitness:" + evalResults[i] + ")");
+			} else {
+				System.out.println("");				
+			}
+		}		
+		
+	}
 
+	public static int fitnessFunction(boolean[] schedule) {
+		
+		int fitness = 0;
+		
+		for (int i=0; i<INTERVALS_PER_DAY; i++) {
+			if (schedule[i]) {
+				fitness++;
+			}
+		}
+				
+		return fitness;
+		
+	}
+	
+	public static void evaluate(boolean[][] population) {
+		
+		// Initialize an array of randomized possible schedules
+		for (int i=0; i<POPULATION; i++) {
+			evalResults[i] = fitnessFunction(population[i]);			
+		}				
+		
+	}
+
+	public static void printEvalResults() {
+		
+		// Initialize an array of randomized possible schedules
+		for (int i=0; i<POPULATION; i++) {
+			System.out.print(evalResults[i] + ",");	
+		}			
+				
+	}
+	
+	public static int indexOfBestFit() {
+		
+		int val=0;
+		int index=-1;
+		
+		for (int i=0; i<evalResults.length; i++) {
+			if (evalResults[i] > val) {
+				val = evalResults[i];
+				index = i;
+			}
+		}
+		
+		return index;
+		
+	}
+	
+	public static boolean[][] selection(boolean[][] population) {
+		
+		boolean[][] newPopulation = new boolean[POPULATION][INTERVALS_PER_DAY];
+		int bestFitPos = indexOfBestFit();
+		int i; // counter
+		int selectedPos;
+		
+		// Create a list of eligible positions to be picked from
+		List<Integer> schedPos = new ArrayList<Integer>();
+		for (i=0; i<POPULATION; i++) {
+			schedPos.add(i);
+		}
+		
+		// Put the best fit solution in first and remove it from the list
+		newPopulation[0] = population[bestFitPos];
+		schedPos.remove(bestFitPos);
+		
+		// Pick the next 4 "mates" randomly from the remaining population
+		for (i=1; i<(POPULATION/2); i++) {
+			selectedPos = (int) (Math.random() * (schedPos.size() + 1));
+			newPopulation[i] = population[selectedPos];
+			schedPos.remove(selectedPos);
+		}		
+		
+		return newPopulation;
+		
+	}
+	
+	public static void crossover(boolean[][] population) {
+		
+		int crossoverPoint = 0;
+		int i;
+		
+		crossoverPoint = (int)(Math.random() * INTERVALS_PER_DAY);
+		
+		// Create the last half of the next generation by breeding the elite/best fit population
+		// member with each of the 
+		for (i=(POPULATION/2); i<POPULATION; i++) {
+			
+		}
+		
+	}
+	
+	public static boolean[] crossMembers(boolean[] member1, boolean[2])
+	
 }
